3299974a1ecb9599e782d27e9e294856be1d06e3
Ariful Islam <ariful@arifuls-mac-3.local>
2019-09-22 23:22:59 -0700

    Initial Commit

---
+++ xcodeserver/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  xcodeserver
+//
+//  Created by Ariful Islam on 22/9/19.
+//  Copyright © 2019 parsonal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ xcodeserver/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  xcodeserver
+//
+//  Created by Ariful Islam on 22/9/19.
+//  Copyright © 2019 parsonal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
